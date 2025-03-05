fffdca0bc7f320103dc59e99f9d7dc4fd4c736d3
student <student@tp.local>
2025-03-05 21:52:54 +0300

    'initial'

--- parsing_hh.py
+++ parsing_hh.py
@@ -1,9 +1,23 @@
 import requests
 
-def get_python_vacancy:
+def get_python_vacancy():
     url = 'https://api.hh.ru/vacancies'
     params={
         'per_page': 100,
-        'text': 'python'
+        'text': 'python',
         'area': 2
     }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+    found = data['found']
+    pages = data['pages']
+    vacancies = []
+    for i in range(0, pages+1):
+        params['page'] = i
+        response = requests.get(url, params=params)
+        data =response.json()
+        vacancies.extend(data['items'])
+    pass
+
+get_python_vacancy()
